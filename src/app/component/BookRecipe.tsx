44954ce56fbe10f74f67833c07de447c4d331a98
hadil <hadiltech17@gmail.com>
2024-08-05 16:27:15 +0100

    fix bugs

--- src/app/component/BookRecipe.tsx
+++ src/app/component/BookRecipe.tsx
@@ -12,24 +12,20 @@ const RecipeBook: React.FC<RecipeProps> = ({
   instructions,
 }) => {
   return (
-    <div
-      className="
-    flex items-center justify-center p-16 "
-    >
-      <div className=" w-full max-w-3xl p-10 bg-white rounded-lg shadow-red-800 shadow-sm rtl text-right">
-        <div className=" "></div>
-        <div className="">
-          <h1 className="text-4xl font-bold text-center mb-4">{title}</h1>
-          <h2 className="text-2xl font-semibold mt-6">المكونات</h2>
+    <div className="flex items-center justify-center p-4 md:p-8 lg:p-16">
+      <div className="w-full max-w-3xl p-6 md:p-8 lg:p-10 bg-white rounded-lg shadow-sm shadow-red-500 rtl text-right">
+        <div className=" ">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-4">{title}</h1>
+          <h2 className="text-xl md:text-2xl lg:text-2xl font-semibold mt-6">المكونات</h2>
           <ul className="list-disc pl-5 rtl text-right">
             {ingredients.map((ingredient, index) => (
               <ol key={index} className="text-right">
-              {ingredient}
-            </ol>
+                {ingredient}
+              </ol>
             ))}
           </ul>
-          <h2 className="text-2xl font-semibold mt-6">طريقة التحضير</h2>
-          <p>{instructions}</p>
+          <h2 className="text-xl md:text-2xl lg:text-2xl font-semibold mt-6">طريقة التحضير</h2>
+          <p className="text-base md:text-lg lg:text-lg">{instructions}</p>
         </div>
       </div>
     </div>
